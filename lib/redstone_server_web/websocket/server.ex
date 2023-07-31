3b1bdc854898fd90deaf9e68216e66f9965effbb
hammsvietro <hammsvietro@gmail.com>
2023-07-31 18:59:01 -0300

    Adjust cowboy server structure

--- lib/redstone_server_web/websocket/server.ex
+++ lib/redstone_server_web/websocket/server.ex
@@ -5,28 +5,33 @@ defmodule RedstoneServerWeb.Websocket.Server do
 
   @behaviour :cowboy_websocket
 
-  # WARNING: Handle auth here 
-  def init(req, state) do
+  # TODO: Handle auth here 
+  @impl true
+  def init(req, _state) do
     {:cowboy_websocket, req, %{user: 123}}
   end
 
-  @impl :cowboy_websocket
+  @impl true
   def websocket_init(state) do
     {:ok, state}
   end
 
+  @impl true
   def websocket_handle({:text, message}, state) do
     {:reply, {:text, "hello world"}, state}
   end
 
+  @impl true
   def websocket_handle({:json, _}, state) do
     {:reply, {:text, "hello world"}, state}
   end
 
+  @impl true
   def websocket_info(info, state) do
     {:reply, state}
   end
 
+  @impl true
   def terminate(_reason, _req, _state) do
     :ok
   end
